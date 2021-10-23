f674210f4a40bdfa215e9adf2321a2770dd527c0
Durbek-Gafur <durbek.gafurov@gmail.com>
2021-10-23 14:05:55 -0500

    Tutorial

---
+++ admin/products/views.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializers
+
+# Create your views here.
+class ProductViewSet(viewsets.ViewSet):
+    def list(self,request):  
+        products = Product.objects.all()
+        serializer = ProductSerializers(products, many=True)
+        return Response(serializer.data)
+
+    def create(self,request): 
+        serializer = ProductSerializers(data=request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+
+    def retrieve(self,request,pk=None): 
+        products = Product.objects.get(id=pk)
+        serializer = ProductSerializers(products)
+        return Response(serializer.data)
+
+    def update(self,request,pk=None): 
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializers(instance=product, data=request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self,request,pk=None): 
+        product = Product.objects.get(id=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
